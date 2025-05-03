aa4ac81b313370a00e92296ae09c7763c791b2ba
juniahn-dev <gud0415@gmail.com>
2025-05-02 17:10:21 -0700

    [feat] Add farcaster frame for promote of ludium programs

--- api/index.tsx
+++ api/index.tsx
@@ -1,11 +1,9 @@
-import { Button, Frog, TextInput, parseEther } from "frog";
+import { Button, Frog } from "frog";
 import { devtools } from "frog/dev";
-import { serveStatic } from "frog/serve-static";
 import { pinata } from "frog/hubs";
+import { serveStatic } from "frog/serve-static";
 import { handle } from "frog/vercel";
-import { baseSepolia } from "viem/chains";
-import { betMemeAbi, betMemeContractAddress } from "../betMeme.js";
-import { changeBg, changeTokenInfo } from "../functions/changeTokenInfo.js";
+import { commaNumber } from "../functions/utils.js";
 
 export const app = new Frog({
   assetsPath: "/",
@@ -13,39 +11,29 @@ export const app = new Frog({
   imageOptions: {
     fonts: [
       {
-        name: "Open Sans",
+        name: "Montserrat",
         weight: 400,
         source: "google",
       },
       {
-        name: "Open Sans",
+        name: "Montserrat",
+        weight: 600,
+        source: "google",
+      },
+      {
+        name: "Montserrat",
         weight: 700,
         source: "google",
+        style: "italic",
       },
     ],
   },
   hub: pinata(),
 });
 
-app.frame("/finish", (c) => {
-  return c.res({
-    image: (
-      <img
-        src="https://github.com/juniahn-dev/twitter/blob/main/assets/card-tx-bg.png?raw=true"
-        style={{ position: "absolute", top: 0, left: 0 }}
-        width="1200"
-        height="630"
-      />
-    ),
-    intents: [
-      <Button.Link href="https://betmeme.online">GO to BetMeme</Button.Link>,
-    ],
-  });
-});
-
-app.frame("/approve/:gameId/:token/:time", (c) => {
-  const { gameId, token, time } = c.req.param();
-  const times = new Date(Number(time) * 1000);
+app.frame("/programs/:title/:programId/:deadline/:price/:token", (c) => {
+  const { title, programId, deadline, price, token } = c.req.param();
+  const times = new Date(Number(deadline) * 1000);
   const months = [
     "Jan",
     "Feb",
@@ -63,244 +51,99 @@ app.frame("/approve/:gameId/:token/:time", (c) => {
   const year = times.getFullYear();
   const date = times.getDate();
   const month = months[times.getMonth()];
-  const hour = times.getHours();
-  const minute = times.getMinutes();
-  const fullDate = `${date}, ${month}, ${year} ${hour}:${minute}`;
+  const fullDate = `${month} ${date}, ${year}`;
 
   return c.res({
-    action: `/${gameId}/${token}/time/${time}`,
     image: (
       <>
         <img
-          src={changeBg(token, true).background}
-          style={{ position: "absolute", top: 0, left: 0 }}
+          src="https://github.com/juniahn-dev/twitter/blob/main/assets/card-tx-bg.png?raw=true"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
           width="1200"
           height="630"
         />
-        {changeBg(token, true).mainInfo && (
-          <img
-            src={changeBg(token, true).mainInfo!}
-            style={{ position: "absolute", top: 0, left: 0 }}
-            width="1200"
-            height="630"
-          />
-        )}
-        {!changeBg(token, true).mainInfo && (
-          <img
-            src={changeTokenInfo(token).image}
-            style={{
-              position: "absolute",
-              bottom: "-320",
-              right: "54%",
-              borderRadius: "50%",
-            }}
-            width="150px"
-            height="150px"
-          />
-        )}
-        {!changeBg(token, true).mainInfo && (
-          <div
-            style={{
-              position: "absolute",
-              bottom: "-290",
-              right: "35%",
-              fontSize: 70,
-              fontWeight: "bold",
-              color: "#fff",
-            }}
-          >
-            {changeTokenInfo(token).denom}
-          </div>
-        )}
+        <div
+          style={{
+            position: "absolute",
+            bottom: "-350",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            fontSize: 40,
+            fontWeight: "bold",
+            color: "#fff",
+          }}
+        >
+          {title}
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: "-400",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+
+            display: "flex",
+            alignItems: "center",
+            gap: "10px",
+
+            fontSize: 30,
+            fontWeight: 600,
+            color: "#fff",
+          }}
+        >
+          <span>Price: </span>
+          <span style={{ display: "flex", alignItems: "center", gap: "5px" }}>
+            <span>{commaNumber(price)}</span>
+            <span>{token}</span>
+          </span>
+        </div>
         <div
           style={{
             position: "absolute",
             bottom: "-555",
-            right: "56%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
 
-            padding: "10px",
-            border: "2px solid #fff",
-            borderRadius: "30px",
-
-            fontSize: 25,
-            fontWeight: "bold",
-            color: "#fff",
+            display: "flex",
+            alignItems: "center",
+            gap: "40px",
           }}
         >
-          End Time
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            bottom: "-545",
-            right: "32%",
-            fontSize: 30,
-            fontWeight: "bold",
-            color: "#fff",
-          }}
-        >
-          {fullDate}
-        </div>
-      </>
-    ),
-    intents: [
-      <TextInput placeholder="Approve Amount Value" />,
-      <Button.Transaction target={`/approve/${token}`}>
-        APPROVE
-      </Button.Transaction>,
-    ],
-  });
-});
-
-app.transaction("/approve/:token", (c) => {
-  const { token } = c.req.param();
-  const { inputText } = c;
-
-  const approveTx = c.contract({
-    abi: betMemeAbi,
-    functionName: "approve",
-    args: [betMemeContractAddress, parseEther(inputText || "0")],
-    chainId: `eip155:${baseSepolia.id}`,
-    to: token as `0x${string}`,
-  });
-
-  if (approveTx.status !== "success") {
-    throw new Error("Approve transaction failed");
-  }
-
-  return approveTx;
-});
-
-app.frame("/:gameId/:token/time/:time", (c) => {
-  const { gameId, token, time } = c.req.param();
-  const times = new Date(Number(time) * 1000);
-  const months = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const year = times.getFullYear();
-  const date = times.getDate();
-  const month = months[times.getMonth()];
-  const hour = times.getHours();
-  const minute = times.getMinutes();
-  const fullDate = `${date}, ${month}, ${year} ${hour}:${minute}`;
-
-  return c.res({
-    action: "/finish",
-    image: (
-      <>
-        <img
-          src={changeBg(token).background}
-          style={{ position: "absolute", top: 0, left: 0 }}
-          width="1200"
-          height="630"
-        />
-        {changeBg(token).mainInfo && (
-          <img
-            src={changeBg(token).mainInfo!}
-            style={{ position: "absolute", top: 0, left: 0 }}
-            width="1200"
-            height="630"
-          />
-        )}
-        {!changeBg(token).mainInfo && (
-          <img
-            src={changeTokenInfo(token).image}
-            style={{
-              position: "absolute",
-              bottom: "-320",
-              right: "54%",
-              borderRadius: "50%",
-            }}
-            width="150px"
-            height="150px"
-          />
-        )}
-        {!changeBg(token).mainInfo && (
           <div
             style={{
-              position: "absolute",
-              bottom: "-290",
-              right: "35%",
-              fontSize: 70,
-              fontWeight: "bold",
+              padding: "10px",
+              border: "3px solid #fff",
+              borderRadius: "30px",
+
+              fontSize: 25,
+              fontWeight: 600,
               color: "#fff",
             }}
           >
-            {changeTokenInfo(token).denom}
+            DEADLINE
           </div>
-        )}
-        <div
-          style={{
-            position: "absolute",
-            bottom: "-555",
-            right: "56%",
-
-            padding: "10px",
-            border: "2px solid #fff",
-            borderRadius: "30px",
-
-            fontSize: 25,
-            fontWeight: "bold",
-            color: "#fff",
-          }}
-        >
-          End Time
-        </div>
-        <div
-          style={{
-            position: "absolute",
-            bottom: "-545",
-            right: "32%",
-            fontSize: 30,
-            fontWeight: "bold",
-            color: "#fff",
-          }}
-        >
-          {fullDate}
+          <div
+            style={{
+              fontSize: 30,
+              fontWeight: 600,
+              color: "#fff",
+            }}
+          >
+            {fullDate}
+          </div>
         </div>
       </>
     ),
     intents: [
-      <TextInput placeholder="Bet Value" />,
-      <Button.Transaction target={`/bet/${gameId}/up`}>UP</Button.Transaction>,
-      <Button.Transaction target={`/bet/${gameId}/down`}>
-        DOWN
-      </Button.Transaction>,
+      <Button.Link href={`https://www.ludium.world/programs/${programId}`}>
+        GO check Program!
+      </Button.Link>,
     ],
   });
-});
-
-app.transaction("/bet/:gameId/:status", (c) => {
-  const { gameId, status } = c.req.param();
-  const uint256GameId = BigInt(gameId);
-  const { inputText } = c;
-
-  const betUp = status === "up" ? true : false;
-
-  const betTx = c.contract({
-    abi: betMemeAbi,
-    functionName: "bet",
-    args: [uint256GameId, betUp, parseEther(inputText || "0")],
-    chainId: `eip155:${baseSepolia.id}`,
-    to: betMemeContractAddress,
-  });
-
-  if (betTx.status !== "success") {
-    throw new Error("Bet transaction failed");
-  }
-
-  return betTx;
 });
 
 // @ts-ignore
